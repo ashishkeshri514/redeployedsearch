1ef393c570d921f2b9e47c79f93372efd4121860
ashishkeshri514 <ashish.keshri.514@gmail.com>
2021-09-08 15:08:35 +0530

    Redeploy

---
+++ src/components/Home.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import { key } from "../API";
+import { cx } from "../cx";
+import axios from "axios";
+import Show from "./Show";
+
+const url = "https://searchapi514.herokuapp.com"
+
+const Home = (props) => {
+  const [state, setState] = useState("");
+  const [results, setResults] = useState([]);
+  const [info, setInfo] = useState("");
+
+  /*axios
+   .get(`/allsearch/${state}`)
+        .get('api/profile/')
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        }); */
+  const searchGoogle = async (e) => {
+    e.preventDefault();
+    const resp = await axios({
+      method: 'get',
+      url: `${url}/allsearch/${state}`,
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(function (response) {
+        setResults(JSON.parse(response.request.response).posts);
+    })
+    .catch(function (error) {
+        // console.log(error);
+    });
+    // if (resp) {
+    //         setResults(resp);
+    // }
+    // fetch(
+    //   `/allsearch/${state}`,
+    //   {
+    //     method: "GET",
+    //     headers: {
+    //       "Content-Type": "application/json"
+    //     }
+    //   }
+    // ).then(function (res) {
+    //   console.log(res);
+    // });
+    // try {
+    // const response = await axios
+    //   .get(`/allsearch/${state}`)
+    //   .then(function (res) {
+    //     console.log(res);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+    //   if (response) {
+    //     console.log(response);
+    //     // setResults(response.data.items);
+    //     // setInfo(response.data.searchInformation);
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+  useEffect(() => {
+    async function getResult() {
+          const resp = await axios
+          .get(`${url}/allsearch/${state}`)
+          .then(function (response) {
+              setResults(JSON.parse(response.request.response).posts);
+          })
+          .catch(function (error) {
+              // console.log(error);
+          });
+    }
+    if(state) getResult();
+  }, [state]);
+  return (
+    <div>
+      <div className="App">
+        <h1>Search</h1>
+        <form onSubmit={searchGoogle}>
+          <input
+            type="text"
+            style={{ margin: "10px", padding: "1px 4px 1px 4px" }}
+            onChange={(e) => {
+              setState(e.target.value);
+            }}
+            value={state}
+          />
+          <input type="submit" value="Search" />
+        </form>
+      </div>
+      {/* {console.log(results)} */}
+      <Show results={results} info={info} />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Show.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../styles.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const url = "https://searchapi514.herokuapp.com"
+
+const Show = (props) => {
+  const { results, info } = props;
+  const apiCall = (id)=>{
+     axios.get(`${url}/search/${id}`)
+        .then(function (response) {
+            // console.log("s");
+        })
+        .catch(function (error) {
+            // console.log(error);
+        });
+  }
+
+  return (
+    <div className="searchresult">
+      <div style={{ color: "silver", fontWeight: "normal", fontSize: "14px" }}>
+        {info ? `Total results: ${info.totalResults}` : ""}
+      </div>
+
+      {results.length > 0
+        ? results.map((result, index) => (
+            <div className="showdetails" key={index}>
+              {/* {console.log({title:result.title,displayLink:result.displayLink,snippet:result.snippet})} */}
+              <div className="showlink">{result.displayLink}</div>
+              <Link to="./static" onClick={() => apiCall(result._id)}>{result.title} </Link>
+              <div>{result.snippet}</div>
+              <div style={{color:"grey"}}>This page has been visited: {result.count} times</div>
+            </div>
+          ))
+        : ""}
+    </div>
+  );
+};
+
+export default Show;
